ff8640f1363ca618065736c945669c74bd4141b9
ssarnobat <ss401533@gmail.com>
2015-02-02 20:27:15 -0800

    Minor

--- server.js
+++ server.js
@@ -7,16 +7,22 @@ var app = express();
 
 app.get('/files/tree', function(request, res) {
 
+	console.log("begin");
 	var location = request.query.locations;//"/media/sarnobat/e/Sridhar/Photos/camera phone photos/iPhone/";
+	console.log("Locations: specified by client: " + location);
 	{
-		exec("tree -J '" + location + "'", function (error, stdout, stderr) { 
+		var command = "tree -J '" + location + "'";
+		console.log("Command executed:\n\t" + command);
+		exec(command, function (error, stdout, stderr) { 
 			if (error != null) {
 				puts("ERROR:" + stderr);
 			}
-			var lines = stdout.split( "\n" )
+			var lines = stdout;//stdout.split( "\n" );
+			puts(lines);
 			puts(lines.length);
 			var json = {};
-			json[location] =lines ;
+			json[location] = JSON.parse(lines) ;
+			console.log(json);
 
     		        res.header("Access-Control-Allow-Origin", "*");
 			res.send(json);
